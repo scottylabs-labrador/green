c131faa99f3bbf01bf9a981906609e7fa04aa956
AutumnQiu99 <autumn.t.qiu@gmail.com>
2024-11-23 17:36:22 -0500

    Change color of buttons

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -8,7 +8,7 @@ type LinkButtonProps = {
 
 const LinkButton = ({ buttonLabel, page }: LinkButtonProps) => {
   return (
-    <Pressable className="bg-gray-500 rounded-lg py-3 px-6 self-center hover:bg-gray-600 mb-4">
+    <Pressable className="bg-emerald-900 rounded-lg py-3 px-6 self-center hover:bg-emerald-950 mb-4">
       <Link href={page} className="flex items-center justify-center">
         <Text className="text-white text-lg font-semibold">{buttonLabel}</Text>
       </Link>
