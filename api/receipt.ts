d0ef42eaca854cf0e7f141e59ce3fdf6b783f769
AutumnQiu99 <autumn.t.qiu@gmail.com>
2025-02-22 17:55:07 -0500

    match receipt and grocery items

--- api/receipt.ts
+++ api/receipt.ts
@@ -1,3 +1,19 @@
-function cleanData(data) {
-    
-}
+import Fuse from "fuse.js";
+
+export const matchWords = (receipt, groceryList, threshold = 0.3) => {
+    const fuse = new Fuse(groceryList, { threshold });
+    const usedWords = new Set();
+
+    return receipt.map(word => {
+        const results = fuse.search(word);
+        const bestMatch = results.find(r => !usedWords.has(r.item));
+
+        if (bestMatch) {
+            usedWords.add(bestMatch.item);
+            return { word, bestMatch: bestMatch.item };
+        }
+        return { word, bestMatch: null };
+    });
+};
+
+// https://chatgpt.com/c/67b11891-1e2c-8009-86bf-ee0c7c6b02a8
