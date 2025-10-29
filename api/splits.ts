a5dc054eedb848a267e5aa2e8f2e6994f8a7e2fe
kzhao214 <kzhao214@gmail.com>
2025-10-28 21:40:39 -0400

    Fix price calculations based on splits

--- api/splits.ts
+++ api/splits.ts
@@ -1,17 +1,32 @@
-export const calculateSplits = recipeInfo => {
-  let splitvalues = {};
-  for (const item in recipeInfo) {
-    const itemInfo = recipeInfo[item];
-    const price = itemInfo.price;
-    const splits = itemInfo.splits;
-    const splitprice = parseFloat((price / splits.length).toFixed(2));
-    for (const user of splits) {
-      if (user in splitvalues) {
-        splitvalues[user] = splitvalues[user] + splitprice;
+import { ReceiptItems } from "@db/types";
+
+export const calculateSplits = (receiptItems: ReceiptItems) => {
+  if (!receiptItems) {
+    return {};
+  }
+
+  let splitPrices: Record<string, number> = {};
+
+  for (const itemId in receiptItems) {
+    const item = receiptItems[itemId];
+    const price = item.price || 0;
+    const splits = item.splits || {};
+    
+    let totalSplitCount = 0;
+    for (const userId in splits) {
+      totalSplitCount += splits[userId];
+    }
+
+    for (const userId in splits) {
+      const userSplitCount = splits[userId];
+      const splitPrice = parseFloat(((price * userSplitCount) / totalSplitCount).toFixed(2));
+      if (userId in splitPrices) {
+        splitPrices[userId] = splitPrices[userId] + splitPrice;
       } else {
-        splitvalues[user] = splitprice;
+        splitPrices[userId] = splitPrice;
       }
     }
   }
-  return splitvalues;
+  
+  return splitPrices;
 };
